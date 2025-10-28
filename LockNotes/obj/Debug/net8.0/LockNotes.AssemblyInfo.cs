623c1fb3c56891ac56f5207acfd18d8f89862408
Ben Salkield <ben@salkield.uk>
2025-10-28 22:03:29 +0000

    Added Note.cs Model

--- LockNotes/obj/Debug/net8.0/LockNotes.AssemblyInfo.cs
+++ LockNotes/obj/Debug/net8.0/LockNotes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LockNotes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e45a718b46a8c4206f25d885a685c56dcb48a10")]
 [assembly: System.Reflection.AssemblyProductAttribute("LockNotes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LockNotes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
